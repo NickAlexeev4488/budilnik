54bfb9c15d685cb097f9103cd0f96b88a9f79685
Nikolai Alexeev <N.Alexeev@seventest.ru>
2023-12-19 18:31:08 +0300

    got navigation

--- lib/alarmList/Main.dart
+++
@@ -1,9 +0,0 @@
-import 'package:budilnik/MainImports.dart';
-
-import 'package:budilnik/alarmList/AlarmList.dart';
-// import 'package:intl/intl.dart';
-
-void main() => runApp(MaterialApp(
-      theme: ThemeData(primaryColor: Colors.black),
-      home: alarmList(),
-    ));
